127d8fa6fc7d17ee1c9dfa5a2effbc0b015afe14
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-01-21 01:44:37 +0530

    index.js formatted

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
-const app = require('./src/server')
-const port = parseInt(process.env.PORT, 10) || 3000
+require('dotenv').config();
+const app = require('./src/server');
 
-server.listen(port)
+const port = parseInt(process.env.PORT, 10) || 3000;
+
+app.listen(port);
